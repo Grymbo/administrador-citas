34764ed0933d62339ef86a910e8696570aee1f5b
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-01-25 08:57:04 -0600

    se modifico el codigo de manera que se reiniciaria el formulario cunado se guardaban los datos, i gual manera se crea el reset del formulario

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -14,6 +14,11 @@ class Citas {
     constructor() {
         this.citas = []
     }
+
+    agregarCita(cita) {
+        this.citas = [...this.citas, cita];
+        console.log(this.citas);
+    }
 }
 
 class UI {
@@ -84,4 +89,27 @@ function nuevaCita(e) {
         ui.imprimirAlerta('Todos los campos son obligatorios', 'error');
         return;
     }
+
+    // Generar un id Unico
+    citaObj.id = Date.now();
+
+    // Creando una nueva cita
+    administrarCitas.agregarCita({...citaObj});
+
+    // Reiniciar el obejto para la validacion
+    reiniciarObjeto();
+
+    // Reiniciar el formulario
+    formulario.reset();
+
+    // Mostrar el HTML de las citas
+}
+
+function reiniciarObjeto() {
+    citaObj.mascota = '';
+    citaObj.propietario = '';
+    citaObj.telefono = '';
+    citaObj.fecha = '';
+    citaObj.hora = '';
+    citaObj.sintomas = '';
 }
