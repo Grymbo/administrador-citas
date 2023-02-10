805728619d39cf620a85bba5c43c6d91db3e278f
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-02-10 09:56:09 -0600

    se edita correctamente el objeto

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -24,6 +24,10 @@ class Citas {
     eliminarCita(id) {
         this.citas = this.citas.filter( cita => cita.id !== id )
     }
+
+    editarCita(citaActualizada) {
+        this.citas = this.citas.map( cita => cita.id === citaActualizada.id ? citaActualizada : cita);
+    }
 }
 
 class UI {
@@ -162,6 +166,9 @@ function nuevaCita(e) {
         ui.imprimirAlerta('Editando Correctamente');
 
         // Pasar el objeto de la cita a edicion
+        administrarCitas.editarCita({...citaObj});
+
+        // Regresar el texto del boton a su estado original
         formulario.querySelector('button[type="submit"]').textContent = 'Crear Cita';
         
         // Quitar modo edicion
